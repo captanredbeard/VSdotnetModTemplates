38de975a9716e28fdf53b3b9de8a042cb146f45d
Captanredbeard <captanredbeard2@gmail.com>
2025-08-30 22:58:39 -0500

    Added a modconfig folder

---
+++ VSModTemplates/templates/VintageStoryMod/_ProjectName_/ModConfig/_ProjectName-ModConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Millwright.ModConfig
+{
+    public class ModConfig
+    {
+        public static ModConfig Loaded { get; set; } = new ModConfig();
+
+        #if (AddSampleCode)
+        public bool ExampleBoolean { get; set; } = true;
+        public double ExampleDouble { get; set; } = 10.5;
+        #endif
+    }
+}
